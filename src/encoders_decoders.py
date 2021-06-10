MSE correct scaling

src/encoders_decoders.py
@@ -15,9 +15,9 @@ def initialize_categorical_params(c0,sigma0,q0):
 def initialize_bernoulli_params(N,x_min,x_max):
     #Initialize parameters arranging centers equally spaced in the range x_min x_max,
     # and the width as 5 times the spacing between centers
-    cs = torch.nn.Parameter(torch.arange(x_min,x_max,(x_max-x_min)/N))[None,:]
-    log_sigmas = torch.log(torch.ones(N)*5*(x_max-x_min)/N)[None,:]
-    As = torch.nn.Parameter(torch.ones(N))[None,:]
+    cs = torch.nn.Parameter(torch.arange(x_min,x_max,(x_max-x_min)/N)[None,:])
+    log_sigmas = torch.nn.Parameter(torch.log(torch.ones(N)*(x_max-x_min)/N)[None,:])
+    As = torch.nn.Parameter(torch.ones(N)[None,:])
     return cs,log_sigmas,As
 class CategoricalEncoder(torch.nn.Module):
     #It returns for each stimulus x a vector of (unnormalized) probabilities (i.e.logits) of activation for
@@ -56,9 +56,9 @@ class BernoulliEncoder(torch.nn.Module):
 def initialize_MOG_params(N,x_min,x_max):
     #Initialize parameters arranging centers equally spaced in the range x_min x_max,
     # and the width as 5 times the spacing between centers
-    mus = torch.nn.Parameter(torch.arange(x_min,x_max,(x_max-x_min)/N))[:,None]
-    log_sigmas = torch.log(torch.ones(N)*5*(x_max-x_min)/N)[:,None]
-    qs = torch.nn.Parameter(torch.ones(N))[:,None]
+    mus = torch.nn.Parameter(torch.arange(x_min,x_max,(x_max-x_min)/N)[:,None])
+    log_sigmas = torch.nn.Parameter(torch.log(torch.ones(N)*(x_max-x_min)/N)[:,None])
+    qs = torch.nn.Parameter(torch.ones(N)[:,None])
     return qs,mus,log_sigmas
 class MoGDecoder(torch.nn.Module):
     #Decoder as a mixture of Gaussians, parameters are q(memberships), mean and variances.

src/losses.py
@@ -2,19 +2,19 @@
 import torch
 import torch.nn.functional as F
 import numpy as np
-from encoders_decoders import *
+from src.encoders_decoders import *
 # %%
 ### Categorical latent space 
 
 def simplified_loss(x,decoder):
     # Compute loss function of MoG decoder in the ideal case of optimal encoder
     # given the decoder , as log(Σ_j q_j q(x|j))
-    inv_sigma2 = torch.exp(-2*decoder.log_sigmass.transpose(0,1)) #[1,N]
-    mp = decoder.muss.transpose(0,1)*inv_sigma2
+    inv_sigma2 = torch.exp(-2*decoder.log_sigmas.transpose(0,1)) #[1,N]
+    mp = decoder.mus.transpose(0,1)*inv_sigma2
     # x has shape [bsize,1]d
-    logq_x_j = -0.5*(x**2)*inv_sigma2 + x*mp - 0.5*mp*decoder.muss.transpose(0,1)
-    -np.log(np.sqrt(2*np.pi)) -decoder.log_sigmas.transpose(0,1)
-    + torch.LogSoftmax(decoder.q,dim=1)
+    logq_x_j = -0.5*(x**2)*inv_sigma2 + x*mp - 0.5*mp*decoder.mus.transpose(0,1)-\
+    np.log(np.sqrt(2*np.pi)) -decoder.log_sigmas.transpose(0,1) +\
+    F.log_softmax(decoder.qs.transpose(0,1),dim=1)
     logZ = -torch.logsumexp(logq_x_j,dim=1)
     return logZ.mean() 
 
@@ -23,17 +23,17 @@ def distortion_cat(x,encoder,decoder):
     # E_x[ sum_j p(j|x)*log(q(x|j))]
     p_j_x = F.softmax(encoder(x),dim=1)
     #Compute log(q(x|j))
-    inv_sigma2 =torch.exp(-2*(decoder.log_sigmass.transpose(0,1)))
-    mp = (decoder.mus.transpose(0,1)*inv_sigma2)
-    logq_x_j = -0.5*(x**2)*inv_sigma2 + x*mp - 0.5*mp*decoder.mus.transpose(0,1)
-    - np.log(np.sqrt(2*np.pi))-decoder.log_sigmas.transpose(0,1)
+    inv_sigma2 =torch.exp(-2*(decoder.log_sigmas.transpose(0,1)))
+    mp = decoder.mus.transpose(0,1)*inv_sigma2
+    logq_x_j = -0.5*(x**2)*inv_sigma2 + x*mp - 0.5*mp*decoder.mus.transpose(0,1)-\
+    np.log(np.sqrt(2*np.pi))-decoder.log_sigmas.transpose(0,1)
     D = -((p_j_x*logq_x_j).sum(dim=1)).mean()
     return D
 
 def rate_cat(x,encoder,decoder):
     p_tilde = encoder(x)
-    R = (F.softmax(p_tilde)*(F.log_softmax(p_tilde) 
-    - F.log_softmax(decoder.q))).sum(dim=1).mean()
+    R = (F.softmax(p_tilde,dim=1)*(F.log_softmax(p_tilde,dim=1)-\
+    F.log_softmax(decoder.qs.transpose(0,1),dim=1))).sum(dim=1).mean()
     return R
 #
 # %%
@@ -49,10 +49,10 @@ def distortion_gaussian(x,encoder,decoder,lat_samp=10,tau=0.5):
     D = -logq_x_r.mean()
     return D
 def distortion(x,encoder,decoder):
-  p_j_x = encoder(x_data)
+  p_j_x = encoder(x)
   inv_sigma2 = 1/(decoder.sigma.transpose(0,1))**2
   mp = (decoder.mu.transpose(0,1)*inv_sigma2)
-  logq_x_j = -0.5*(x_data**2)@inv_sigma2 + (x_data@mp) - 0.5*(mp)*(decoder.mu.transpose(0,1))-         torch.log(np.sqrt(2*np.pi)*decoder.sigma.transpose(0,1))
+  logq_x_j = -0.5*(x**2)@inv_sigma2 + (x_data@mp) - 0.5*(mp)*(decoder.mu.transpose(0,1))-         torch.log(np.sqrt(2*np.pi)*decoder.sigma.transpose(0,1))
   loss = -((F.softmax(p_j_x))*logq_x_j).sum(dim=1).mean()
   return loss
 