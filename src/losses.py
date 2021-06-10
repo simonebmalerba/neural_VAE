177f00eb730a0c00c3b4437370239069cfb3a5dd
driante <s.driante@gmail.com>
2021-06-10 16:27:44 +0200

    MSE in losses

--- src/losses.py
+++ src/losses.py
@@ -35,8 +35,14 @@ def rate_cat(x,encoder,decoder):
     R = (F.softmax(p_tilde,dim=1)*(F.log_softmax(p_tilde,dim=1)-\
     F.log_softmax(decoder.qs.transpose(0,1),dim=1))).sum(dim=1).mean()
     return R
-#
+def MSE_cat(x,decoder,encoder=None):
+    if encoder is None:
+        encoder = CategoricalEncoder(decoder.mus.transpose(0,1),torch.exp(decoder.log_sigmas).transpose(0,1),decoder.qs.transpose(0,1))
+    l_j_x = encoder(x)
+    mse = (F.softmax(l_j_x,dim=1)*((x-decoder.mus.transpose(0,1))**2 + torch.exp(2*decoder.log_sigmas.transpose(0,1)))).sum(dim=1).mean()
+    return mse
 # %%
+#Bernoulli latent space
 def distortion_gaussian(x,encoder,decoder,lat_samp=10,tau=0.5):
     p_r_x = encoder(x)
     bsize,N = p_r_x.shape
@@ -48,20 +54,5 @@ def distortion_gaussian(x,encoder,decoder,lat_samp=10,tau=0.5):
     logq_x_r = -0.5*(x**2)*inv_sigma2_dec + x*mp - 0.5*mu_dec*mp - 0.5*torch.log(2*np.pi*sigma2_dec)
     D = -logq_x_r.mean()
     return D
-def distortion(x,encoder,decoder):
-  p_j_x = encoder(x)
-  inv_sigma2 = 1/(decoder.sigma.transpose(0,1))**2
-  mp = (decoder.mu.transpose(0,1)*inv_sigma2)
-  logq_x_j = -0.5*(x**2)@inv_sigma2 + (x_data@mp) - 0.5*(mp)*(decoder.mu.transpose(0,1))-         torch.log(np.sqrt(2*np.pi)*decoder.sigma.transpose(0,1))
-  loss = -((F.softmax(p_j_x))*logq_x_j).sum(dim=1).mean()
-  return loss
 
-def Dkl(x_data,encoder,decoder):
-  p_j_x = encoder(x_data)
-  dkl = ((F.softmax(p_j_x))*(F.log_softmax(p_j_x) - F.log_softmax(decoder.q))).sum(dim=1).mean() 
-  return dkl
 
-def MSE(x_data,encoder,decoder):
-  p_j_x = encoder(x_data)
-  mse = ((F.softmax(p_j_x))*(x_data**2 + decoder.mu.transpose(0,1)**2 -2*x_data*decoder.mu.transpose(0,1) + decoder.sigma.transpose(0,1)**2)).sum(dim=1).mean()
-  return mse
