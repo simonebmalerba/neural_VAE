492b9597abbac0a2cc28384ad5dd8b4cfed5e980
simonebmalerba <s.driante@gmail.com>
2022-04-08 07:31:17 +0000

    Running on Server

--- distortion_rate.py
+++ distortion_rate.py
@@ -54,7 +54,7 @@ N_SAMPLES =5000
 BATCH_SIZE = 100
 #lr = 1e-4
 #Generate dataset
-p_x = torch.distributions.normal.Normal(0,5) 
+p_x = torch.distributions.log_normal.LogNormal(1,1) 
 x_samples = p_x.sample((N_SAMPLES,))[:,None]     #Points used for model train
 x_test = p_x.sample((40000,))[:,None]            #Points used for model test
 x_tsorted,_ = x_test.sort(dim=0)
@@ -66,15 +66,15 @@ x_data = torch.utils.data.DataLoader(x_samples,batch_size=BATCH_SIZE)
 #Initialize model parameters
 #Iterate over different R^*
 resume = {}
-RtVec = np.linspace(0.2,3.1,num=15)
+RtVec = np.linspace(0.3,2.5,num=15)
 #RtVec = [0.5,1]
 for Rt in RtVec:
-    if Rt < 1.:
+    if Rt < 1.3:
         lr = 1e-4
     else:
         lr=1e-3
     print(f"Rate = {Rt}||lr = {lr}")
-    enc = BernoulliEncoder(N,x_min-1,x_max+1,x_sorted)
+    enc = BernoulliEncoder(N,x_min-1,x_max+1,x_sorted,w=3)
     dec = MLPDecoder2n(N,M)     #Decoder
     q = rate_ising(N)           #Prior
     q.J.register_hook(lambda grad: grad.fill_diagonal_(0))
@@ -87,7 +87,7 @@ for Rt in RtVec:
                 'history' : history,
                 'x_test'  : x_tsorted
     }
-PATH = os.getcwd() + "/data/N_prior_N=10_q=Ising_#1.pt"
+PATH = os.getcwd() + "/data/LN_prior_N=10_q=Ising_#4.pt"
 torch.save(resume, PATH)
 # %%
 
